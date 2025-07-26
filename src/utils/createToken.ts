0755cc098cf6ad1a8ce23459b0ce96dcfce4a857
Karan Dhiman <136183168+kulbirdhiman@users.noreply.github.com>
2025-07-26 16:42:28 +0530

    add

--- src/utils/createToken.ts
+++ src/utils/createToken.ts
@@ -11,7 +11,7 @@ const createToken = (res: Response, id: any) => {
       httpOnly: true,
       secure: true,
       sameSite: "none",
-      domain: ".kayhanaudio.com.au", // ✅ good
+      domain: ".kayhanaudio.com.au", 
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
