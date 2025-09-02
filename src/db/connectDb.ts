c69dcc9d0975871197ec155cabd7af77c028c96a
Karan Dhiman <136183168+kulbirdhiman@users.noreply.github.com>
2025-09-02 19:23:38 +0530

    some changes

--- src/db/connectDb.ts
+++ src/db/connectDb.ts
@@ -7,7 +7,7 @@ const connectDb = async () => {
     await sequelize.authenticate();
     console.log("✅ Database connected successfully.");
     setupAssociations();
-    sequelize.sync()
+    sequelize.sync({alter : true})
   .then(() => {
     console.log("✅ DB altered successfully");
   })
