964defcf5834fca21f2c4b61c3caed97efb15309
kulbirdhiman <dhimankulbir1825@gmail.com>
2026-02-10 13:05:50 +0530

    increase limit

--- src/index.ts
+++ src/index.ts
@@ -70,8 +70,9 @@ app.use(
   })
 );
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // routes
 
